95eaf320bde84d8073ed95531f1a84085c951963
Gayal Kuruppu <gayal@fcodelabs.com>
2019-09-15 22:27:38 +0530

    added descriptive variable names and printed only the function name

--- AvailabilityCombinationsNew.py
+++ AvailabilityCombinationsNew.py
@@ -18,45 +18,45 @@ def availability(availabilityClass):
 # functions for mean time to failure
 
 def f1_n(x):
-    return ([x, 'n'])
+    return (x)
 
 
 def f1_nlogn(x):
-    return ([x * math.log10(x), 'nlogn'])
+    return (x * math.log10(x))
 
 
 def f1_sqrt_n(x):
-    return ([math.sqrt(x), 'sqrt n'])
+    return (math.sqrt(x))
 
 
 def f1_sqrt_n_cube(x):
-    return ([pow(x, 1.5), 'n^1.5'])
+    return (pow(x, 1.5))
 
 
 def f1_n_pow_2over3(x):
-    return ([pow(x, 2 / 3), 'n^(2/3)'])
+    return (pow(x, 2 / 3))
 
 
 # functions to mean time to recover
 
 def f2_1_over_n(x):
-    return ([1 / x, '1/n'])
+    return (1 / x)
 
 
 def f2_1_over_nlogn(x):
-    return ([1 / (x * math.log10(x)), '1/nlogn'])
+    return (1 / (x * math.log10(x)))
 
 
 def f2_1_over_sqrt_n(x):
-    return ([1 / math.sqrt(x), '1/(sqrt n)'])
+    return (1 / math.sqrt(x))
 
 
 def f2_1_over_sqrt_n_cube(x):
-    return ([pow(x, -1.5), '1/n^(1.5)'])
+    return (pow(x, -1.5))
 
 
 def f2_1_over_n_pow_2over3(x):
-    return ([pow(x, -2 / 3), '1/n^(2/3)'])
+    return (pow(x, -2 / 3))
 
 
 # lists of column tags and their content
@@ -67,33 +67,38 @@ mttrSW = [3600, 1800, 300, 60, 30]
 function1 = [f1_n, f1_nlogn, f1_sqrt_n, f1_sqrt_n_cube, f1_n_pow_2over3]
 function2 = [f2_1_over_n, f2_1_over_nlogn, f2_1_over_sqrt_n, f2_1_over_sqrt_n_cube, f2_1_over_n_pow_2over3]
 
-# list containing all the scenarios with its overall availability
 data = []
 
 for i in itertools.product(nodes, availabilityClassHW, availabilityClassSW, mttrSW, function1, function2):
+    '''
+    nodes_count = temp[0]
+    avialability_hw = temp[1]
+    avialability_sw = temp[2]
+    mttr_sw = temp[3]
+    f1 = temp[4]
+    f2 = temp[5]
+    availability
+    '''
+
     scenario = list(i)
     # getting the availability of the hardware for the availability class for hardware
     aClassHW = availability(scenario[1])
     # getting the availability of the software for the availability class for software
     aClassSW = availability(scenario[2])
     # calculate the availability of the regarding scenario
-    overallAvailability = pow((aClassHW * mttf_sw(aClassSW, scenario[3]) * scenario[4](scenario[0])[0] /
-                               (mttf_sw(aClassSW, scenario[3]) * scenario[4](scenario[0])[0] + scenario[3] *
-                                scenario[5](scenario[0])[0])), scenario[0])
-    # function 1 naming
-    scenario[4] = scenario[4](0.1)[1]
-    # function 2 naming
-    scenario[5] = scenario[5](0.1)[1]
+    overallAvailability = pow((aClassHW * mttf_sw(aClassSW, scenario[3]) * scenario[4](scenario[0]) /
+                               (mttf_sw(aClassSW, scenario[3]) * scenario[4](scenario[0]) + scenario[3] *
+                                scenario[5](scenario[0]))), scenario[0])
     overallAvailabilityClass = availabilityClass(overallAvailability)
     scenario.append(overallAvailabilityClass)
-    data.append(scenario)
+    data.append([scenario[0], scenario[1], scenario[2], scenario[3], scenario[4].__name__, scenario[5].__name__, overallAvailabilityClass]) # I have fixed so that you can see function name
 
 # data here should be the list containing all the data
 
 df = pd.DataFrame(data, columns=['Nodes', 'HW Availability', 'SW Availability', 'MTTR SW', 'function 1',
                                  'function 2', 'Overall availability'])
 print(df.head())
-df.to_csv('Availability.csv')
+df.to_csv('AvailabilityS.csv')
 
 # isClass_3 = df['a_sw'] == 3
 # filtered_data = data[isClass_3]
